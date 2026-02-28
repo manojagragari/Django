d0d222242f99b844d19c4aa44d761ed3a21a86f4
Manoj <manoj@example.com>
2026-02-28 12:40:48 +0530

    haha

--- backend/shop/permissions.py
+++ backend/shop/permissions.py
@@ -1,9 +1,20 @@
-from rest_framework.permissions import BasePermission
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
-class IsAdminUserGroup(BasePermission):
+class SalePermission(BasePermission):
+
     def has_permission(self, request, view):
-        if not request.user.is_authenticated:
-            return False
+        role = request.user.role
 
-        return request.user.groups.filter(name="Admin").exists()
-    
+        # Admin → full access
+        if role == "admin":
+            return True
+
+        # Manager → only view (GET)
+        if role == "manager":
+            return request.method in SAFE_METHODS
+
+        # Staff → only create sale (POST)
+        if role == "staff":
+            return request.method == "POST"
+
+        return False
--- backend/shop/views/sale_views.py
+++ backend/shop/views/sale_views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from ..models import Sale
 from ..serializers.sale_serializer import SaleSerializer
+from ..permissions import SalePermission
 
 
 class SaleListCreateView(generics.ListCreateAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
+    permission_classes = [IsAuthenticated, SalePermission]
 
 
 class SaleDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Sale.objects.all()
     serializer_class = SaleSerializer
+    permission_classes = [IsAuthenticated, SalePermission]
